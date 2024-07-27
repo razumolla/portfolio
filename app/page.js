59ec5a48d848e9e08b980f5141c83ebc591d13c0
razumolla <rajuahmed9344@gmail.com>
2024-07-27 18:07:51 +0600

    experience

--- app/page.js
+++ app/page.js
@@ -2,12 +2,14 @@ import Image from "next/image";
 import HeroSection from "./components/homepage/Hero";
 import About from "./components/homepage/About";
 import Contact from "./components/homepage/contact";
+import Experience from "./components/homepage/Experience";
 
 export default function Home() {
   return (
     <>
       <HeroSection />
       <About />
+      <Experience />
       <Contact />
     </>
   );
---
+++ utils/data/experience.js
@@ -0,0 +1,20 @@
+export const experiences = [
+  {
+    id: 1,
+    title: 'Software Developer',
+    company: "HK IT Limited",
+    duration: "(May 2023 - Present)"
+  },
+  {
+    id: 2,
+    title: "FullStack Web Developer",
+    company: "Goinnovior Limited",
+    duration: "(Jun 2023 - April 2023)"
+  },
+  {
+    id: 3,
+    title: "Self Employed",
+    company: "Code and build something in everyday.",
+    duration: "(Jan 2018 - Present)"
+  }
+]
