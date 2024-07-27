cbbfb1aff711ddd256735ec6786ca13d300dda71
razumolla <rajuahmed9344@gmail.com>
2024-07-27 15:35:06 +0600

    contact

--- app/page.js
+++ app/page.js
@@ -1,12 +1,14 @@
 import Image from "next/image";
 import HeroSection from "./components/homepage/Hero";
 import About from "./components/homepage/About";
+import Contact from "./components/homepage/contact";
 
 export default function Home() {
   return (
     <>
       <HeroSection />
       <About />
+      <Contact />
     </>
   );
 }
