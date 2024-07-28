eac160d0c8ede692703ed97d72494bd9317d533d
razumolla <rajuahmed9344@gmail.com>
2024-07-28 13:10:11 +0600

    skills

--- app/page.js
+++ app/page.js
@@ -3,12 +3,14 @@ import HeroSection from "./components/homepage/Hero";
 import About from "./components/homepage/About";
 import Contact from "./components/homepage/contact";
 import Experience from "./components/homepage/Experience";
+import Skills from "./components/homepage/Skills";
 
 export default function Home() {
   return (
     <>
       <HeroSection />
       <About />
+      <Skills />
       <Experience />
       <Contact />
     </>
