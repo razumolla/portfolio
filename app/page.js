e46f672ea7b77f924feae3ebabe93025cf02f174
razumolla <rajuahmed9344@gmail.com>
2024-07-28 14:17:58 +0600

    education

--- app/page.js
+++ app/page.js
@@ -4,6 +4,7 @@ import About from "./components/homepage/About";
 import Contact from "./components/homepage/contact";
 import Experience from "./components/homepage/Experience";
 import Skills from "./components/homepage/Skills";
+import Education from "./components/homepage/Education";
 
 export default function Home() {
   return (
@@ -12,6 +13,7 @@ export default function Home() {
       <About />
       <Skills />
       <Experience />
+      <Education />
       <Contact />
     </>
   );
---
+++ utils/data/educations.js
@@ -0,0 +1,20 @@
+export const educations = [
+  {
+    id: 1,
+    title: "B.Sc in CSE",
+    duration: "2022 - Present",
+    institution: "European University of Bangladesh",
+  },
+  {
+    id: 2,
+    title: "Diploma in Computer Technology",
+    duration: "2018 - 2020",
+    institution: "Govt. Technical School & College, Rajbari",
+  },
+  {
+    id: 3,
+    title: "Secondary School Certificate",
+    duration: "2016",
+    institution: "Satair High School",
+  }
+]
