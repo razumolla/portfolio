8d05e48c5afd06105135f128630f63377ae330b4
razumolla <rajuahmed9344@gmail.com>
2024-07-29 11:37:23 +0600

    project issue solved

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,5 @@
 import { Inter } from "next/font/google";
-import "./globals.css";
+import "./css/globals.scss";
 import "./css/card.scss";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
