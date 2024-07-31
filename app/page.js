55f50472147d057d952f87745237aa64c70a1f09
razumolla <rajuahmed9344@gmail.com>
2024-07-31 12:01:37 +0600

    blog

--- app/page.js
+++ app/page.js
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import Hero from "./components/homepage/Hero";
 import About from "./components/homepage/About";
 import Contact from "./components/homepage/contact";
@@ -6,8 +5,26 @@ import Experience from "./components/homepage/Experience";
 import Skills from "./components/homepage/Skills";
 import Education from "./components/homepage/Education";
 import Projects from "./components/homepage/projects";
+import Blog from "./components/homepage/blog";
+import { personalData } from "@/utils/data/personal-data";
 
-export default function Home() {
+async function getData() {
+  const res = await fetch(`https://dev.to/api/articles?username=${personalData.devUsername}`)
+
+  if (!res.ok) {
+    throw new Error('Failed to fetch data')
+  }
+
+  const data = await res.json();
+
+  const filtered = data.filter((item) => item?.cover_image).sort(() => Math.random() - 0.5);
+
+  return filtered;
+};
+
+export default async function Home() {
+  const blogs = await getData();
+
   return (
     <>
       <Hero />
@@ -16,6 +33,7 @@ export default function Home() {
       <Experience />
       <Projects />
       <Education />
+      {/* <Blog blogs={blogs} /> */}
       <Contact />
     </>
   );
